2b682c692fbb7c6c798c64bb58f7cf0fe87f5c68
Dhruvi <dhruviradadiya453@gmail.com>
2025-09-19 17:21:13 +0530

    index

--- index.js
+++ index.js
@@ -12,11 +12,25 @@ app.use(
       "http://localhost:3000", 
       "https://lead-crm-frontend-new.vercel.app", 
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
+// Additional CORS headers for production
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
